5f7f161257a8cfcf8433e64eed4be2049be0b602
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-26 16:39:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+import { initFlowbite } from "flowbite";
+export default{
+  created() {
+    initFlowbite();
+  },
+}
+</script>
+
+<template>
+  <header>
+ 
+    <nav class="">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    
+    
+  </header>
+  <main>
+    <RouterView />
+  </main>
+
+</template>
+
+<style>
+
+</style>
